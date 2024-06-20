916c751c52db0c10e2fcb15817b8a01e6c59a0a3
digitalMeansArg <digitalmeanshub@gmail.com>
2024-06-19 23:27:30 -0300

    ajustes

--- src/public/inicio.js
+++ src/public/inicio.js
@@ -16,11 +16,17 @@ botonburger.addEventListener("click", reveal)
 
 function reveal (){
   let displayedNavbar=document.getElementById("reveal")
+  let hideWhileNavBar1= document.getElementById("hideOnNavBar1")
+  let hideWhileNavBar2= document.getElementById("hideOnNavBar2")
 
   if(displayedNavbar.style.display=="block"){
     displayedNavbar.style.display="none"
+    hideWhileNavBar1.style.display=""
+    hideWhileNavBar2.style.display=""
   }else{
     displayedNavbar.style.display="block"
+    hideWhileNavBar1.style.display="none"
+    hideWhileNavBar2.style.display="none"
   }
 
 
